1b57c589442169c2ccf8357f776bb02cf9eae186
mario <mario.briseno7@gmail.com>
2023-12-21 18:53:28 -0600

    printing now works better but still req/resp flow not working

--- src/tracer/mod.rs
+++ src/tracer/mod.rs
@@ -1,7 +1,8 @@
+use std::io::{self, Write};
 use dns_lookup::{ lookup_host, lookup_addr };
-use std::{str, thread};
+use std::{str, thread, string};
 use icmp;
-use std::net::{IpAddr, Ipv4Addr, SocketAddr, UdpSocket};
+use std::net::{IpAddr, Ipv4Addr, Ipv6Addr, SocketAddr, UdpSocket};
 use std::time::Duration;
 
 const LOWER_LIMIT_PORT: u32 = 33_434;
@@ -10,12 +11,6 @@ const UDP_PORT: u16 = 50_000;
 const LOCAL_IP: IpAddr = IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1));
 const LOCAL_SOCK_ADDRS: SocketAddr = SocketAddr::new(LOCAL_IP, UDP_PORT);
 const MSG: &[u8] = "Hello listeners".as_bytes();
-
-
-fn get_host_and_port(host: &str, port: u32) -> String {
-    let host_ip = lookup_host(host).unwrap()[0];
-    format!("{0}:{1}", host_ip, port)
-}
 
 
 pub struct TRouter {
@@ -63,40 +58,68 @@ impl TRouter {
 }
 
 
-fn msg_formater(hop_num: u32, host: &str, hop_ip: &str) {
-    println!("{}", format!("{0}\t{1} ({2})", hop_num, host, hop_ip))
+fn get_host_and_port(host: &str, port: u32) -> String {
+    let host_ips = lookup_host(host).unwrap();
+    let mut host_ip: Option<&IpAddr> = None::<&IpAddr>;
+    for ip_value in host_ips.iter() {
+        if let IpAddr::V4(_) = ip_value {
+            host_ip = Some(ip_value);
+        }
+    }
+    if host_ip.is_none() {
+        panic!("No IPv4 address was found");
+    }
+    format!("{0}:{1}", host_ip.unwrap(), port)
+}
+
+
+fn msg_formater(host: &str, hop_ip: &str) {
+    println!("{}", format!("{} ({})", host, hop_ip))
 }
 
 
 pub fn run_tracerouter(host: &str) {
-
-    thread::spawn(|| {
-        let mut counter = 1;
-        let mut ping_socket = icmp::IcmpSocket::connect(LOCAL_IP).unwrap();
-        let mut inter_ip: String;
-        loop {
-            let mut icmp_msg = [0u8; 20];
-            let (num_nutes, hop_ip) = ping_socket.recv_from(&mut icmp_msg).unwrap();
-            match lookup_addr(&hop_ip) {
-                Ok(hop_name) => {inter_ip=hop_name.to_string()},
-                Err(e) => {inter_ip=hop_ip.to_string()}
-            };
-            msg_formater(counter, &inter_ip, &hop_ip.to_string());
-            thread::sleep(Duration::from_millis(100));
-            counter += 1
-        }
-    });
-
+    
+    let mut ping_socket = icmp::IcmpSocket::connect(LOCAL_IP).unwrap();
+    ping_socket.set_read_timeout(Some(Duration::new(1, 0)));
     let mut router = TRouter::new(host);
     router.init_message();
     let mut socket = UdpSocket::bind(LOCAL_SOCK_ADDRS).expect(
         "Couldn't connect to host"
     );
-    socket.set_ttl(1).expect("Couldn't set TTL");
-    router.set_port(&mut socket);
-    for i in 2..64 {
-        socket.set_ttl(i).expect("Could not set TTL");
-        &socket.send(MSG);
-        thread::sleep(Duration::from_millis(50));
+    let mut counter = 1;
+    let mut inter_ip: String;
+    for i in 1..64 {
+        print!("{}\t", i);
+        io::stdout().flush().unwrap();
+        &socket.set_ttl(i).expect("Could not set TTL");
+        if i == 1 {
+            router.set_port(&mut socket);
+        } else {
+            // HANDLE ERRORS AND RETRY IT
+            &socket.send(MSG);
+        }
+        let mut icmp_msg = [0u8; 20];
+        for j in 0..3 {
+            let recv_result = ping_socket.recv_from(&mut icmp_msg);
+            if recv_result.is_err() {
+                if j == 2 {
+                    println!("*");
+                    break;
+                } else {
+                    print!("*  ");
+                    io::stdout().flush().unwrap();
+                }
+                continue
+            }
+            let (num_nutes, hop_ip) = recv_result.unwrap();
+            match lookup_addr(&hop_ip) {
+                Ok(hop_name) => {inter_ip=hop_name.to_string()},
+                Err(e) => {inter_ip=hop_ip.to_string()}
+            };
+            msg_formater(&inter_ip, &hop_ip.to_string());
+            break;
+        }
+        counter += 1;
     }   
 }
